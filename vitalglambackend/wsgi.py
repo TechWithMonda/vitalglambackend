9df217aa0f3df0effba757d3d992a5243a9d7abf
MondaSamuel <samwelmonda48@gmail.com>
2025-03-11 18:58:34 +0300

    Auto-run migrations and create superuser

--- vitalglambackend/wsgi.py
+++ vitalglambackend/wsgi.py
@@ -1,17 +1,13 @@
-"""
-WSGI config for vitalglambackend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vitalglambackend.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
+
+django.setup()
+
+# Run migrations on startup
+from django.core.management import call_command
+call_command("migrate")
 
 application = get_wsgi_application()
-
